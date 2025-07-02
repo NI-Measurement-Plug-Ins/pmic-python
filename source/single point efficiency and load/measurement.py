a4d61ac68ff8fabbaa951c872aa22775a6b602bf
Giriharan Ravichandran <giriharan.ravichandran@ni.com>
2025-07-02 20:16:15 +0530

    fix: update meas config values

--- source/single point efficiency and load/measurement.py
+++ source/single point efficiency and load/measurement.py
@@ -24,16 +24,16 @@ measurement_service = nims.MeasurementService(  # Check if class name is still M
 @measurement_service.configuration('Source delay', nims.DataType.Double, 0.005)
 # Aperture time is the period during which an ADC reads the voltage or current on a power supply or SMU
 @measurement_service.configuration('Aperture time', nims.DataType.Double, 0.005)
-@measurement_service.configuration('Nominal output voltage', nims.DataType.Double, 1.3)
+@measurement_service.configuration('Nominal output voltage', nims.DataType.Double, 3.3)
 # Source Settings
-@measurement_service.configuration('Source resource name', nims.DataType.String, 'PPS')
-@measurement_service.configuration('Source current limit', nims.DataType.Double, 1.0)
+@measurement_service.configuration('Source resource name', nims.DataType.String, '')
+@measurement_service.configuration('Source current limit', nims.DataType.Double, 25.0)
 @measurement_service.configuration('Source maximum power', nims.DataType.Double, 50.0)
 # Load Settings
-@measurement_service.configuration('Load resource name', nims.DataType.String, 'E-load')
-@measurement_service.configuration('Load voltage limit range', nims.DataType.Double, 5.0)
-@measurement_service.configuration('Source Voltage', nims.DataType.Double, 0)
-@measurement_service.configuration('Load Current', nims.DataType.Double, 0)
+@measurement_service.configuration('Load resource name', nims.DataType.String, '')
+@measurement_service.configuration('Load voltage limit range', nims.DataType.Double, 6.0)
+@measurement_service.configuration('Source Voltage', nims.DataType.Double, 2)
+@measurement_service.configuration('Load Current', nims.DataType.Double, 0.001)
 # configure outputs
 @measurement_service.output('Status', nims.DataType.String)
 @measurement_service.output('Load currents', nims.DataType.Double)
